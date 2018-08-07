f8b71ef0b7500efc20ead3df0f7911cb2238ac1f
Thomas Shockman <tom.shockman@gmail.com>
2018-08-07 16:44:37 -0400

    Fix cors

--- src/main/java/com/shockmanepsteindownie/backend/services/UserService.java
+++ src/main/java/com/shockmanepsteindownie/backend/services/UserService.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shockmanepsteindownie.backend.models.User;
 import com.shockmanepsteindownie.backend.repositories.UserRepository;
 
-@CrossOrigin(origins="http://localhost:3000", allowCredentials="true")
+@CrossOrigin(origins={"http://localhost:3000", "https://designs-r-us.herokuapp.com"}, allowCredentials="true")
 @RestController
 public class UserService {
 
