1b300c3c7ec78b0ebe2534c310f1086427d22737
Thomas Shockman <tom.shockman@gmail.com>
2018-08-11 20:37:45 -0400

    Allow individual messages to be accessed

--- src/main/java/com/shockmanepsteindownie/backend/services/MessageService.java
+++ src/main/java/com/shockmanepsteindownie/backend/services/MessageService.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,6 +52,15 @@ public class MessageService {
 			messageRepository.save(message);
 		}
 		return ResponseEntity.ok(true);
+	}
+	
+	@GetMapping("/api/message/{mid}")
+	public ResponseEntity<Message> getMessage(@PathVariable("mid") int mid) {
+		Optional<Message> opt = messageRepository.findById(mid);
+		if (!opt.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.ok(opt.get());
 	}
 	
 	@PostMapping("/api/profile/message/{username}")
